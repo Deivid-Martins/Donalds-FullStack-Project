54b58f32923243f4a8b71cfcab82f9abf9d91ea1
Deivid Martins <deividrocha2005@gmail.com>
2025-02-22 10:50:36 -0300

    feat: add scroll to products details page

--- src/app/[slug]/menu/[productId]/components/products-header.tsx
+++ src/app/[slug]/menu/[productId]/components/products-header.tsx
@@ -7,38 +7,40 @@ import { useRouter } from "next/navigation";
 
 import { Button } from "@/components/ui/button";
 
-interface ProductsHeaderProps {
-  product: Pick<Product, 'name' | 'imageUrl'>
+interface ProductHeaderProps {
+  product: Pick<Product, "name" | "imageUrl">;
 }
 
-export default function ProductsHeader ({ product }: ProductsHeaderProps) {
+const ProductHeader = ({ product }: ProductHeaderProps) => {
   const router = useRouter();
-
   const handleBackClick = () => router.back();
-
   return (
-    <div className="relative w-full h-[300px]">
-        <Button
-          variant="secondary"
-          size="icon"
-          className="absolute left-4 top-4 z-50 rounded-full"
-          onClick={handleBackClick}
-        >
-          <ChevronLeftIcon />
-        </Button>
-        <Button
-          variant="secondary"
-          size="icon"
-          className="absolute right-4 top-4 z-50 rounded-full"
-        >
-          <ScrollTextIcon />
-        </Button>
-        <Image
-          src={product.imageUrl} 
-          alt={product.name}
-          fill
-          className="object-contain"
-        />
-      </div>
+    <div className="relative min-h-[300px] w-full">
+      <Button
+        variant="secondary"
+        size="icon"
+        className="absolute left-4 top-4 z-50 rounded-full"
+        onClick={handleBackClick}
+      >
+        <ChevronLeftIcon />
+      </Button>
+
+      <Image
+        src={product.imageUrl}
+        alt={product.name}
+        fill
+        className="object-contain"
+      />
+
+      <Button
+        variant="secondary"
+        size="icon"
+        className="absolute right-4 top-4 z-50 rounded-full"
+      >
+        <ScrollTextIcon />
+      </Button>
+    </div>
   );
-}
+};
+
+export default ProductHeader;
--- src/app/[slug]/menu/[productId]/page.tsx
+++ src/app/[slug]/menu/[productId]/page.tsx
@@ -3,38 +3,38 @@ import { notFound } from "next/navigation";
 import { db } from "@/lib/prisma";
 
 import ProductDetails from "./components/product-details";
-import ProductsHeader from "./components/products-header";
+import ProductHeader from "./components/products-header";
 
 interface ProductPageProps {
-  params: Promise<{ slug: string, productId: string }>
+  params: Promise<{ slug: string; productId: string }>;
 }
 
-export default async function ProductPage ({ params }: ProductPageProps) {
+const ProductPage = async ({ params }: ProductPageProps) => {
   const { slug, productId } = await params;
-  
-  const product = await db.product.findUnique({ where: { id: productId },
+  const product = await db.product.findUnique({
+    where: { id: productId },
     include: {
       restaurant: {
         select: {
           name: true,
           avatarImageUrl: true,
-          slug: true
-        }
-      }
-    }});
-
-  if(product == null) {
+          slug: true,
+        },
+      },
+    },
+  });
+  if (!product) {
     return notFound();
   }
-  
-  if(product.restaurant.slug.toUpperCase() !== slug.toUpperCase()) {
+  if (product.restaurant.slug.toUpperCase() !== slug.toUpperCase()) {
     return notFound();
   }
-
   return (
     <div className="flex h-full flex-col">
-      <ProductsHeader product={product} />
+      <ProductHeader product={product} />
       <ProductDetails product={product} />
     </div>
   );
-}
+};
+
+export default ProductPage;
