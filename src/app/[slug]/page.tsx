77a2d72eee06b9b3e4950b8c3aa9514a3e0eddb8
Deivid Martins <deividrocha2005@gmail.com>
2025-02-21 22:07:23 -0300

    feat: add initial restaurant page and menu page

---
+++ src/app/[slug]/page.tsx
@@ -0,0 +1,56 @@
+import Image from "next/image";
+import { notFound } from "next/navigation";
+
+import { db } from "@/lib/prisma";
+
+import ConsumptionMethodOption from "./components/consumption-method-option";
+
+interface RestaurantsPageProps {
+  params: Promise<{ slug: string }>;
+}
+
+export default async function RestaurantPage({ params }: RestaurantsPageProps) {
+  const { slug } = await params;
+  const restaurant = await db.restaurant.findUnique({ where: { slug } });
+  if (!restaurant) {
+    return notFound();
+  }
+  return (
+    <div className="flex h-screen flex-col items-center justify-center px-6 pt-24">
+      {/* Logo and Title */}
+      <div className="flex flex-col items-center gap-2">
+        <Image
+          src={restaurant.avatarImageUrl}
+          alt={restaurant.name}
+          width={82}
+          height={82}
+        />
+        <h2 className="font-semibold">{restaurant.name}</h2>
+      </div>
+      {/* Welcome message */}
+      <div className="space-y-2 pt-24 text-center">
+        <h3 className="text-2xl font-semibold">Seja bem-vindo!</h3>
+        <p className="opacity-55">
+          Escolha como prefere aproveitar sua refeição. Estamos aqui para
+          oferecer praticidade e sabor em cada detalhe!
+        </p>
+      </div>
+      <div className="grid grid-cols-2 gap-4 pt-14">
+        <ConsumptionMethodOption
+          slug={slug}
+          buttonText="Para comer aqui"
+          imageAlt="Para comer aqui"
+          imageUrl="/hamburguer.png"
+          option="DINE_IN"
+        />
+        <ConsumptionMethodOption
+          slug={slug}
+          buttonText="Para levar"
+          imageAlt="Para levar"
+          imageUrl="/bag.png"
+          option="TAKEAWAY"
+        />
+      </div>
+    </div>
+  );
+}
