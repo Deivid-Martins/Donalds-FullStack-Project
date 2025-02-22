65ed6535e74b2fc914e9bf97de581fe6c5de297a
Deivid Martins <deividrocha2005@gmail.com>
2025-02-22 11:32:10 -0300

    feat: add initial add to cart function to cart context

--- src/app/[slug]/menu/components/cart-sheet.tsx
+++ src/app/[slug]/menu/components/cart-sheet.tsx
@@ -1,22 +1,33 @@
 import { useContext } from "react";
 
-import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle } from "@/components/ui/sheet";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
 
 import { CartContext } from "../contexts/cart";
 
 export default function CartSheet () {
-  const { isOpen, toggleCart } = useContext(CartContext);
-
+  const { isOpen, toggleCart, products } = useContext(CartContext);
   return (
     <Sheet open={isOpen} onOpenChange={toggleCart}>
       <SheetContent>
         <SheetHeader>
-          <SheetTitle></SheetTitle>
+          <SheetTitle>Are you absolutely sure?</SheetTitle>
           <SheetDescription>
-
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
           </SheetDescription>
         </SheetHeader>
+        {products.map((product) => (
+          <h1 key={product.id}>
+            {product.name} - {product.quantity}
+          </h1>
+        ))}
       </SheetContent>
     </Sheet>
   );
-}
+};
--- src/app/[slug]/menu/contexts/cart.tsx
+++ src/app/[slug]/menu/contexts/cart.tsx
@@ -3,20 +3,23 @@
 import { Product } from "@prisma/client";
 import { createContext, ReactNode, useState } from "react";
 
-interface CartProduct extends Product {
+interface CartProduct
+  extends Pick<Product, "id" | "name" | "price" | "imageUrl"> {
   quantity: number;
 }
 
-export interface ICartContexrt {
+export interface ICartContext {
   isOpen: boolean;
   products: CartProduct[];
   toggleCart: () => void;
+  addProduct: (product: CartProduct) => void;
 }
 
-export const CartContext = createContext<ICartContexrt>({
+export const CartContext = createContext<ICartContext>({
   isOpen: false,
   products: [],
   toggleCart: () => {},
+  addProduct: () => {},
 });
 
 export function CartProvider ({ children }: { children: ReactNode }) {
@@ -24,16 +27,37 @@ export function CartProvider ({ children }: { children: ReactNode }) {
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
   const toggleCart = () => {
-    setIsOpen(!isOpen);
-  }
-
+    setIsOpen((prev) => !prev);
+  };
+  const addProduct = (product: CartProduct) => {
+    const productIsAlreadyOnTheCart = products.some(
+      (prevProduct) => prevProduct.id === product.id,
+    );
+    if (!productIsAlreadyOnTheCart) {
+      return setProducts((prev) => [...prev, product]);
+    }
+    setProducts((prevProducts) => {
+      return prevProducts.map((prevProduct) => {
+        if (prevProduct.id === product.id) {
+          return {
+            ...prevProduct,
+            quantity: prevProduct.quantity + product.quantity,
+          };
+        }
+        return prevProduct;
+      });
+    });
+  };
   return (
-    <CartContext.Provider value={{
-      isOpen,
-      products,
-      toggleCart,
-    }}>
+    <CartContext.Provider
+      value={{
+        isOpen,
+        products,
+        toggleCart,
+        addProduct,
+      }}
+    >
       {children}
     </CartContext.Provider>
   );
-}
+};
